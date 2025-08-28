42418ad28353fbb33fe6bc586bb7c711aab3b283
ethanhillman <echillman252@gmail.com>
2025-08-28 17:26:58 +0000

    feat- loaded data on to my file

---
+++ lesson_10/libraries/src/loaders/ethan_hillman_loader.ts
@@ -0,0 +1,29 @@
+import csv from 'csv-parser';
+import fs from 'fs';
+import { MediaItem } from '../models/media_items';
+import { Loader } from './loader.js';
+export class EthanHillmanLoader implements Loader {
+  getLoaderName(): string {
+    return 'ethanhillman';
+  }
+
+  async loadData(): Promise<MediaItem[]> {
+    const mediaItems = await this.loadMediaItems();
+
+    console.log(`Loaded ${mediaItems.length} media items`);
+
+    return [...mediaItems.values()];
+  }
+
+  async loadMediaItems(): Promise<MediaItem[]> {
+    const mediaItems = [];
+    const readable = fs
+      .createReadStream('data/media_items.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { id, title, type, release_year } = row;
+      mediaItems.push(new MediaItem(id, title, type, parseInt(release_year)));
+    }
+    return mediaItems;
+  }
+}
