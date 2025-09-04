67148f0a9ac07792987f9ecd392558d1781b391e
ethanhillman <echillman252@gmail.com>
2025-09-04 18:58:08 +0000

    feat- Implemented my code

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,8 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +10,16 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+        map.put(s.charAt(i), i);
+    }
+    int diff = 0;
+    for (int i = 0; i < t.length(); i++) {
+        char currChar = t.charAt(i);
+        int currIdx = map.get(currChar);
+        diff += Math.abs(i - currIdx);
+    }
+    return diff;
   }
 }
